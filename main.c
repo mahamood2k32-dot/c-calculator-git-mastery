2de79f6faa5ed9cf7548c9ee8c4d9016da3b1f1f
Mahamood2k32-dot <Mahamood2k32@gmail.com>
2026-01-29 03:55:30 -0500

    Added subtraction operation -main.c

--- main.c
+++ main.c
@@ -3,7 +3,31 @@
 
 int main()
 {
-	printf("Welcome to C Calculator\n");
+
+	int a, b;
+	char op;
+
+	printf("Enter '+' or '-'  operator\n");
+
+	scanf("%c",&op);
+	
+	
+	printf("Enter two numbers\n");
+	scanf("%d %d",&a,&b);
+	
+	if(op == '+')
+	{
+		printf("\nResult = %d\n", a+b);
+	}
+	else if(op == '-')
+	{
+		printf("\nResult = %d\n", a-b);
+	}
+	else
+	{
+		printf("Invalid operator");
+        }
+
 	return 0;
 
 }
